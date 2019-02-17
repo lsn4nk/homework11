801857a8fc6cbc8efb8247a858dd14953aa3275d
lsn4nk <lsn4nk1@gmail.com>
2019-02-17 19:57:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const model = new Students();
 const view = new StudentsView('#main-container');
 const controller = new StudentsController(model, view);
 
-controller.setViewContext('#main-container');
 controller.showStudents();
+controller.setViewContext('#main-container');
